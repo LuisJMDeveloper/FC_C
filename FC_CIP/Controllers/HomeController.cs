de3a5d63642634f0ccda1f87ad27a3cb3843e52d
LuisJM <ljarrieta81@misena.edu.co>
2023-04-04 19:27:46 -0500

    Se agregó una clase UsuarioInstructor la cual tiene los campos usados en el procedimiento de saveUserValidation de sql server
    Se agregó HomeController la acción
    [HttpPost]
    RegistrarInstructor(UsuarioInstructor oUsuario)
    
    Se agregó a la vist index del HomeController
    la sección scrip
    funcion Registrar
    
    Todo lo agregado aún ni ha sido probado

--- FC_CIP/Controllers/HomeController.cs
+++ FC_CIP/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Services.Description;
+using System.Windows.Forms;
+using FC_CIP.Models;
+using FC_CIP.Models.TablasCombinadas;
 
 namespace FC_CIP.Controllers
 {
@@ -16,6 +21,37 @@ namespace FC_CIP.Controllers
         public ActionResult Registrar()
         {
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult RegistrarInstructor(UsuarioInstructor oUsuario)
+        {
+            bool respuesta = true;
+
+            try
+            {
+                using (FC_CIP_BDEntities db = new FC_CIP_BDEntities())
+                {
+
+                    db.saveUserValidation(
+                        oUsuario.us_nid,
+                        oUsuario.us_password,
+                        oUsuario.us_name,
+                        oUsuario.us_lastname,
+                        oUsuario.us_email,
+                        oUsuario.us_phone
+                        );
+                    
+                    db.SaveChanges();
+                }
+            }catch(SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                respuesta = false;
+            }
+
+            return Json( new {resultado = respuesta}, JsonRequestBehavior.AllowGet);
+
         }
     }
 }
