b0620b3faa9ba25748bce6ce86e8635b765e7096
LuisJM <ljarrieta81@misena.edu.co>
2023-04-05 17:38:27 -0500

    Código de reporte coordinador trasladado con exito

---
+++ FC_CIP/Controllers/CoordinadorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FC_CIP.Controllers
+{
+    public class CoordinadorController : Controller
+    {
+        // GET: Coordinador
+        public ActionResult CoordinadorIndex()
+        {
+            return View();
+        }
+    }
+}
